595d4b62056ff55ba8b40d8b5b2c926face66a4d
Mohamad Hammal <MohammadHammal10@gmail.com>
2022-06-19 23:46:08 +0300

    Start with template driven form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,21 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit() {}
 
-  constructor() {
-
-
+  submit(form: NgForm) {
+    console.log(form.value, form.valid);
   }
-
-  ngOnInit() {
-
-  }
-
 }
